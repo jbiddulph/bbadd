bf1d0c32792ad8b93515532171d3c91ceb54344e
jbiddulph <john.mbiddulph@gmail.com>
2024-05-09 23:03:26 +0100

    changed key

--- composables/useSupabase.ts
+++ composables/useSupabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlbWFmZWhwb2tua2Jlamxia3NhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTI2Nzk4MywiZXhwIjoyMDMwODQzOTgzfQ.4NfTOLlIv1qCs_wBP2bt0i1SbyYV84h6HVp55oV2s54"
-const SUPABASE_URL = 'https://qemafehpoknkbejlbksa.supabase.co'
+const SUPABASE_KEY = process.env.SUPABASE_KEY
+const SUPABASE_URL = process.env.SUPABASE_URL
 // const supabaseKey = process.env.SUPABASE_KEY
 
 const useSupabase = () => {
