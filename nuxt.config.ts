33be6ce88265fd19f906f6a65f46492f1e156941
jbiddulph <john.mbiddulph@gmail.com>
2024-05-09 23:19:50 +0100

    added to gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui',
   ],
+  runtimeConfig: {
+    public: {
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+        SUPABASE_KEY: process.env.SUPABASE_KEY,
+    },
+},
   devtools: { enabled: true }
 })
